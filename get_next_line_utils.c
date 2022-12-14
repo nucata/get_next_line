f6173f7a5ffc6c480b4af6eea0febf8da4c2fe25
rdragan <rdragan@student.42heilbronn.de>
2022-12-14 17:46:22 +0100

    added strchr

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: rdragan <rdragan@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 16:37:47 by rdragan           #+#    #+#             */
-/*   Updated: 2022/12/14 17:38:39 by rdragan          ###   ########.fr       */
+/*   Updated: 2022/12/14 17:45:59 by rdragan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,26 @@ size_t	len(char *s)
 	size_t	i;
 
 	i = 0;
-	if (!s)	
+	if (!s)
 		return (0);
 	while (s[i])
 		i++;
 	return (i);
 }
 
-char	*j_strchr(char *s, char c);
+char	*j_strchr(char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	return (NULL);
+}
+
 char	*j_strjoin(char *s1, char *s2);
 char	*j_substr(char *s1, size_t start);
