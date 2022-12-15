008733d849ed534d3de0196398674d96e988ca99
rdragan <rdragan@student.42heilbronn.de>
2022-12-15 16:09:22 +0100

    created indexof

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rdragan <rdragan@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 16:37:31 by rdragan           #+#    #+#             */
-/*   Updated: 2022/12/15 15:55:13 by rdragan          ###   ########.fr       */
+/*   Updated: 2022/12/15 16:09:06 by rdragan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,5 @@ size_t	len(char *s);
 char	*j_strchr(char *s, char c);
 char	*j_strjoin(char *s1, char *s2);
 char	*j_substr(char *s1, size_t start, size_t end);
-
+int		indexof(char *s1, char c);
 #endif
