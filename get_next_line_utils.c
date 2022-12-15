38f482f46073a7d8e0d0adc65b4e43bb26c440cc
rdragan <rdragan@student.42heilbronn.de>
2022-12-15 15:47:27 +0100

    created substr

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: rdragan <rdragan@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 16:37:47 by rdragan           #+#    #+#             */
-/*   Updated: 2022/12/14 17:45:59 by rdragan          ###   ########.fr       */
+/*   Updated: 2022/12/15 15:44:51 by rdragan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ size_t	len(char *s)
 	size_t	i;
 
 	i = 0;
-	if (!s)
-		return (0);
-	while (s[i])
+	while (s && s[i])
 		i++;
 	return (i);
 }
@@ -38,5 +36,49 @@ char	*j_strchr(char *s, char c)
 	return (NULL);
 }
 
-char	*j_strjoin(char *s1, char *s2);
-char	*j_substr(char *s1, size_t start);
+char	*j_strjoin(char *s1, char *s2)
+{
+	char	*new;
+	size_t	ls1;
+	size_t	ls2;
+	int		i;
+	int		j;
+
+	ls1 = len(s1);
+	ls2 = len(s2);
+	new = malloc((ls1 + ls2 + 1) * sizeof(char));
+	if (!new)
+		return (NULL);
+	i = -1;
+	while (s1 && s1[++i])
+		new[i] = s1[i];
+	j = -1;
+	while (s2 && s2[++j])
+		new[i + j] = s2[j];
+	new[i + j] = '\0';
+	free(s1);
+	return (new);
+}
+
+char	*j_substr(char *s1, size_t start)
+{
+	char	*new;
+	size_t	ls1;
+	size_t	new_len;
+	int		i;
+
+	ls1 = len(s1);
+	new_len = ls1 - start;
+	new = malloc((new_len + 1) * sizeof(char));
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (s1[start + i])
+	{
+		new[i] = s1[start + i];
+		i++;
+	}
+	new[i] = '\0';
+	free(s1);
+	return (NULL);
+}
